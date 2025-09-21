9e06210a651f547face7ca8a233b96d7708a94c9
Cjking57893 <cody.king04@outlook.com>
2025-09-21 11:42:49 -0400

    Refactored interesects function

--- src/game/piece.py
+++ src/game/piece.py
@@ -1,12 +1,46 @@
 import random
-import src.constants as constants
 
 class Piece:
 
+    I_SHAPE = ((1, 5, 9, 13), (4, 5, 6, 7))
+    Z_SHAPE = ((4, 5, 9, 10), (2, 6, 5, 9))
+    S_SHAPE = ((6, 7, 9, 10), (1, 5, 6, 10))
+    L_SHAPE = ((1, 2, 5, 9), (0, 4, 5, 6), (1, 5, 9, 8), (4, 5, 6, 10))
+    J_SHAPE = ((1, 2, 6, 10), (5, 6, 7, 9), (2, 6, 10, 11), (3, 5, 6, 7))
+    T_SHAPE = ((1, 4, 5, 6), (1, 4, 5, 9), (4, 5, 6, 9), (1, 5, 6, 9))
+    O_SHAPE = ((1, 2, 5, 6))
+        
+    FIGURES = (
+    I_SHAPE,
+    Z_SHAPE,
+    S_SHAPE,
+    L_SHAPE,
+    J_SHAPE,
+    T_SHAPE,
+    O_SHAPE,
+    )
+
+    BLACK = (0, 0, 0)
+    PURPLE = (120, 37, 179)
+    TEAL = (100, 179, 179)
+    BROWN = (80, 34, 22)
+    GREEN = (80, 134, 22)
+    RED = (180, 34, 22)
+    PINK = (180, 34, 122)
+
+    COLORS = (
+        BLACK,
+        PURPLE,
+        TEAL,
+        BROWN,
+        GREEN,
+        RED,
+        PINK,
+    )
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.type = random.randint(0, len(constants.FIGURES) - 1) # pick random shape
-        self.color = random.randint(1, len(constants.COLORS) - 1) # pick random color
+        self.type = random.randint(0, len(self.FIGURES) - 1) # pick random shape
+        self.color = random.randint(1, len(self.COLORS) - 1) # pick random color
         self.rotation = 0 # start unrotated

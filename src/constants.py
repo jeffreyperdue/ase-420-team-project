faee906ac0a6c0747e32732bd9617ee1d8506246
Jeff Perdue <perduej7@mymail.nku.edu>
2025-09-25 11:03:26 -0400

    integrate Cody's changes into main; dealt with merge conflict in constants.py; Changed references to Colors to COLORS in pygame_renderer.py as result of merge resolution

--- src/constants.py
+++ src/constants.py
@@ -2,19 +2,35 @@
 SCREEN_SIZE = (400, 500)
 FPS = 60
 
-# Colors
-Colors = [
-    (0, 0, 0),          # index 0 = empty
-    (120, 37, 179),
-    (100, 179, 179),
-    (80, 34, 22),
-    (80, 134, 22),
-    (180, 34, 22),
-    (180, 34, 122),
-]
+# Grid size for field
+GRID_SIZE = 4
 
+# Starting position for new pieces
+START_X = 3
+START_Y = 0
+
+# Define colors
+BLACK = (0, 0, 0)
+PURPLE = (120, 37, 179)
+TEAL = (100, 179, 179)
+BROWN = (80, 34, 22)
+GREEN = (80, 134, 22)
+RED = (180, 34, 22)
+PINK = (180, 34, 122)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
+
+COLORS = (
+    BLACK,
+    PURPLE,
+    TEAL,
+    BROWN,
+    GREEN,
+    RED,
+    PINK,
+    WHITE,
+    GRAY,
+)
 
 # Cell (block) size in pixels
 CELL_SIZE = 20

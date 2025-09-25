d65e308b39fb33b95167c28df2da74f33dfd8786
Jeff Perdue <perduej7@mymail.nku.edu>
2025-09-25 10:27:29 -0400

    integrate Owen's changes into main

---
+++ src/constants.py
@@ -0,0 +1,20 @@
+# Screen size
+SCREEN_SIZE = (400, 500)
+FPS = 60
+
+# Colors
+Colors = [
+    (0, 0, 0),          # index 0 = empty
+    (120, 37, 179),
+    (100, 179, 179),
+    (80, 34, 22),
+    (80, 134, 22),
+    (180, 34, 22),
+    (180, 34, 122),
+]
+
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+
+# Cell (block) size in pixels
+CELL_SIZE = 20
